f489fc8026aec37e106ae0cb62ddaf85abcf03eb
Fabian Löschner <w1th0utnam3@users.noreply.github.com>
2016-04-05 22:54:55 +0200

    Added copyright notice

--- source/matrixbase.h
+++ source/matrixbase.h
@@ -1,3 +1,22 @@
+/*
+	linear_algebra_containers/column_vector header file
+	Copyright (C) 2015  Fabian Löschner
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #ifndef MATRIXBASE_H
 #define MATRIXBASE_H
 
@@ -77,6 +96,5 @@ inline std::ostream& operator<<(std::ostream& os, const MatrixBase<T,m,n>& mat)
 }
 
 }
-
 
 #endif // MATRIXBASE_H
