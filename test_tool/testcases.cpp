34a793e6a12a41834cff92ffed6d64595978ffd7
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2016-09-13 21:16:43 +0200

    Started to implement tests using Catch

---
+++ test_tool/testcases.cpp
@@ -0,0 +1,42 @@
+﻿//	MIT License
+//
+//	Copyright (c) 2016 Fabian Löschner
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+
+#include "catch.hpp"
+
+#include "matrix.h"
+
+using namespace lin_algebra;
+
+TEST_CASE("Testing Matrix")
+{
+	typedef Matrix<double, 4, 4> mat4x4d;
+	typedef Matrix<double, 4, 1> mat4x1d;
+	typedef Matrix<double, 4, 2> mat4x2d;
+
+	SECTION("Testing initializer list and subscript operator") {
+		const mat4x4d mat{ 1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.,13.,14.,15.,16. };
+
+		for (int i = 0; i < 16; i++) {
+			REQUIRE(mat[i] == i + 1);
+		}
+	}
+}
